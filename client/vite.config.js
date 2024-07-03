1f32d784c6077b33d35f2c4ed4e32acda44a7f06
Pratishtha Sheetal <btech10298.22@bitmesra.ac.in>
2024-07-04 03:43:25 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'client', // Ensure this is correctly set if your source files are in a subdirectory
-  build: {
-    outDir: 'dist',
-  },
 })
