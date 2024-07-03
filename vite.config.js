855b6de1d3b7de87f42d354ea75e4af6fbaecdf1
Pratishtha Sheetal <btech10298.22@bitmesra.ac.in>
2024-07-04 04:14:36 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// /project-root/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'client'),
+  plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, 'client/dist'),
+    rollupOptions: {
+      input: path.resolve(__dirname, 'client/index.html'),
+    },
+  },
+});
