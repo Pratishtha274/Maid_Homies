1ddfe4c1598ae46008717815576ff30d703b6e5e
Pratishtha Sheetal <btech10298.22@bitmesra.ac.in>
2024-07-04 04:12:10 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, 'client'),
   plugins: [react()],
-})
+  build: {
+    outDir: path.resolve(__dirname, 'client/dist'),
+    rollupOptions: {
+      input: path.resolve(__dirname, 'client/index.html'),
+    },
+  },
+});
