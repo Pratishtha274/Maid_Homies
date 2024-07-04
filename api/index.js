ccbc73597bbc7f24fc0bdce32ab97db9b0c84afe
Pratishtha Sheetal <btech10298.22@bitmesra.ac.in>
2024-07-04 21:26:18 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use('/uploads',express.static(__dirname+'/uploads'))
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: 'https://maid-homies-2-git-main-pratishtha-sheetals-projects.vercel.app',
 }))
 
 
