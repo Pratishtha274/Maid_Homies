b565818bbbd4561c12d6b4f9cc39e25fc6226c46
Pratishtha Sheetal <btech10298.22@bitmesra.ac.in>
2024-07-16 19:01:33 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin:'https://maid-homies.vercel.app/',
+  origin:'http://localhost:5173',
 }));
   
 // Serve the built frontend files from the 'dist' folder
